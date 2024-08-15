bf09c8e919e94d9b6986632a507f34a8784da5d6
carolinalibarino <carollibarino2@gmail.com>
2024-08-15 10:29:54 -0300

    react

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import Banner from "./components/Banner";
+import Container from "./components/Container";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Banner image="favoritos" />
+      <Container>
+        <h1>ETEC MCM NOVOOOOOOOO</h1>
+        <p>Olá Mundo!</p>
+      </Container>      
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
