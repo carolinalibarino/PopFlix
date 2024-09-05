20091e4595c96f445cfa2324566e8ef2c8e5ac60
carolinalibarino <carollibarino2@gmail.com>
2024-09-05 12:34:01 -0300

    aula 11

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Banner from "../../components/Banner";
 import Card from "../../components/Card";
 import Category, {categories, filterCategory} from "../../components/Category";
@@ -6,19 +7,42 @@ import Footer from "../../components/Footer";
 import Header from "../../components/Header";
 
 function Home() {
+
+  //let nomeDigitado = ''
+  function pegarNome(event){
+    console.log(event.target.value)
+    //nomeDigitado = event.tagert.value
+    setNome(event.target.value)
+  }
+
+
+  //Hook = gancho
+  const [ nome, setNome ] = useState('Carol')
+
+
   return (
     <>
       <Header/>
       <Banner image="favoritos" />
       <Container>
-        {
+
+      <input
+        type="text"
+        placeholder="Nome"
+        onChange={pegarNome}
+
+      />
+
+      <h2>{nome}</h2>
+
+ {/*        {
           categories.map((category, index)=>
           <Category category={category}>
             { filterCategory(index).map((video)=> <Card id={video.id} key={video.is}/>)}
           </Category>
           
           )
-        }
+        } */}
       </Container>      
       <Footer/>
     </>
