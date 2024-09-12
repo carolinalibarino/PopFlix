5687f0d698dc46148141d24d042f67d2bfab8f8a
carolinalibarino <carollibarino2@gmail.com>
2024-09-12 20:01:46 -0300

    aula 13

---
+++ src/components/SearchVideoList/index.js
@@ -0,0 +1,33 @@
+import styles from "./SearchVideoList.module.css";
+import VideoList from "../VideoList";
+import { useState } from "react";
+
+//filtrando por categoria ou título 
+function filterVideos(videos , searchText){
+    return videos.filter((video)=> videos.category.includes(searchText) || video.title.includes(searchText))
+
+}
+
+function SearchVideoList({ videos }){
+
+    const [ searchText, setSearchText ] = useState('Geografia')
+    const foundVideos = filterVideos(videos, searchText)
+
+
+    return (
+        <section className={styles.container}>
+            <input 
+            type="search" 
+            placeholder="Pesquisar..."
+            value={searchText}
+            onChange={event => setSearchText(event.target.value)}
+            />
+            <VideoList 
+            videos={foundVideos}
+            emptyHeading={`Sem vídeos sobre "${searchText}"`}
+            />
+        </section>
+    );
+}
+
+export default SearchVideoList;
--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -2,8 +2,8 @@ import Header from "../../components/Header";
 import styles from "./Search.module.css";
 import Footer from "../../components/Footer"
 import Container from "../../components/Container";
-import VideoList from "../../components/VideoList";
 import videos from "../../json/videos.json"
+import SearchVideoList from "../../components/SearchVideoList";
 
 
 function Search(){
@@ -14,7 +14,7 @@ function Search(){
                 <section className={styles.search}>
                     <h2>Pesquisar</h2>
 
-                    <VideoList videos={videos}/>
+                    <SearchVideoList videos={videos}/>
                 </section>
             </Container>
             <Footer/>
