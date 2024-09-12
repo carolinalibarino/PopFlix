c2403d24aa91939bc50c36f75360202f0233e745
carolinalibarino <carollibarino2@gmail.com>
2024-09-12 09:10:41 -0300

    aula 12

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,13 +1,22 @@
 import Header from "../../components/Header";
 import styles from "./Search.module.css";
+import Footer from "../../components/Footer"
+import Container from "../../components/Container";
+import VideoList from "../../components/VideoList";
+import videos from "../../json/videos.json"
+
 
 function Search(){
     return(
         <>
             <Header/>
-            <section className={styles.search}>
-                <h2>Pesquisar</h2>
-            </section>
+            <Container>
+                <section className={styles.search}>
+                    <h2>Pesquisar</h2>
+
+                    <VideoList videos={videos}/>
+                </section>
+            </Container>
             <Footer/>
         </>
     );
