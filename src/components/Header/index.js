7b58f95b95f5989714832274021d401bbd8631b5
carolinalibarino <carollibarino2@gmail.com>
2024-09-13 20:59:06 -0300

    aula 16

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -10,7 +10,7 @@ function Header(){
             <nav>
                 <Link to="/">Home</Link>
                 <Link to="/watch">Assisir</Link>
-                <Link to="/search">Pesquisar</Link>
+                <Link to="/favorites">Favoritos</Link>
 
             </nav>
         </header>
